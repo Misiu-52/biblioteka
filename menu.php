52b0aeb1056cbcb451a9e3c584d4da090c577047
Misiu-52 <kozlowskimichal52@gmail.com>
2021-12-23 17:14:22 +0100

    Drobne zmiany

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 <a href="index.php?plik=home" class="menuItem" title="Strona główna"><i class="fas fa-home"></i></a>
 <a href="index.php?plik=sklep" class="menuItem" title="Sklep"><i class="fas fa-shopping-basket"></i></a>
 <a href="index.php?plik=dodaj" class="menuItem" title="Dodaj książkę"><i class="fas fa-plus"></i></a>
-<a href="index.php?plik=znajdz" class="menuItem" title="Wyszukiwarka"><i class="fas fa-search"></i></a>
+<!-- <a href="index.php?plik=znajdz" class="menuItem" title="Wyszukiwarka"><i class="fas fa-search"></i></a> -->
 <a href="index.php?plik=logowanie" class="menuItem" title="Konto"><i class="far fa-user-circle"></i></a>
 
 <?php
